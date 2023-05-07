96c216d3c509dba622888df28cd8f5550a966ac4
Ibtehaz Rafid <IbtehazR@nycstudents.net>
2023-05-07 02:27:19 -0400

    COMMIT UNO

--- src/AClass.java
+++ src/AClass.java
@@ -1,2 +1,89 @@
 public class AClass {
+    public static void main(String[] args) {
+//        AClass d = new AClass();
+//        System.out.println("42 in octal is: ");
+//        System.out.println(d.toOctal(42));
+        // Test the gcd method with positive integers
+        int a = 42;
+        int b = 56;
+        int expectedGcd = 14;
+        int actualGcd = gcd(a, b);
+        System.out.println("gcd(" + a + ", " + b + ") = " + actualGcd);
+
+        // Test the gcd method with negative integers
+        a = -21;
+        b = 14;
+        expectedGcd = 7;
+        actualGcd = gcd(a, b);
+        System.out.println("gcd(" + a + ", " + b + ") = " + actualGcd);
+
+        // Test the gcd method with zero
+        a = 0;
+        b = 14;
+        expectedGcd = 14;
+        actualGcd = gcd(a, b);
+        System.out.println("gcd(" + a + ", " + b + ") = " + actualGcd);
+
+        // Test the lcm method with positive integers
+        int num1 = 12;
+        int num2 = 18;
+        int expectedLcm = 36;
+        int actualLcm = lcm(num1, num2);
+        System.out.println("lcm(" + num1 + ", " + num2 + ") = " + actualLcm);
+
+        // Test the lcm method with negative integers
+        num1 = -15;
+        num2 = -25;
+        expectedLcm = 75;
+        actualLcm = lcm(num1, num2);
+        System.out.println("lcm(" + num1 + ", " + num2 + ") = " + actualLcm);
+
+        // Test the lcm method with zero
+        num1 = 0;
+        num2 = 7;
+        expectedLcm = 0;
+        actualLcm = lcm(num1, num2);
+        System.out.println("lcm(" + num1 + ", " + num2 + ") = " + actualLcm);
+    }
+
+    public String toBinary(int num) {
+        if (num == 0) {
+            return "";
+        }
+        String binary = "";
+        int n = num;
+        if (n % 2 == 0) {
+            return toBinary(n / 2) + "0";
+        } else {
+            return toBinary(n / 2) + "1";
+        }
+    }
+
+    public String toOctal(int num) {
+        if (num == 0) {
+            return "";
+        }
+        int n = num;
+        return toOctal(n / 8) + n % 8;
+    }
+
+    public static int gcd(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (b == 0) {
+            return a;
+        } else {
+            int gcd = gcd(b, a % b);
+            if (a < 0 && b < 0) {
+                return -gcd;
+            } else {
+                return gcd;
+            }
+        }
+    }
+
+    public static int lcm(int a, int b) {
+        int gcd = gcd(a, b);
+        return (a * b) / gcd;
+    }
 }
--- src/Permutations.java
+++ src/Permutations.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Permutations {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+    public static List<String> permutation(String s) {
+        List<String> perms = new ArrayList<>();
+        if (s.length() == 1) {
+            perms.add(s);
+        } else {
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                String rest = s.substring(0, i) + s.substring(i + 1);
+                for (String p : permutation(rest)) {
+                    perms.add(c + p);
+                }
+            }
+        }
+        return perms;
+    }
+
+    public static void main(String[] args) {
+        String s = "abc";
+        List<String> perms = permutation(s);
+        for (String p : perms) {
+            System.out.println(p);
+        }
+    }
 }
